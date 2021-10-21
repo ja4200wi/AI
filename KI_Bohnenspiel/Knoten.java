a7d70ae0ff1279a0ef8d3bea1ee49a32ed745d41
codingPhilipp <57591668+paulpanzer20@users.noreply.github.com>
2021-10-21 16:29:45 +0200

    big update

--- KI_Bohnenspiel/Knoten.java
+++ KI_Bohnenspiel/Knoten.java
@@ -7,6 +7,8 @@ import java.util.Iterator;
 public class Knoten {
 
 
+  static Knoten bestNextMove;
+  static int finalDepth = 6;
   Knoten predecessor;
 
   boolean isLeaf;
@@ -24,7 +26,7 @@ public class Knoten {
     return first;
   }
 
-  Knoten appendKnot(State state, int move){
+  Knoten appendKnot(State state){
     Knoten child = new Knoten();
     child.isLeaf = true;
     child.state = state;
@@ -36,7 +38,83 @@ public class Knoten {
     return child;
   }
 
+  ArrayList<Knoten> getPossibleMovesAsKnots(){
+    ArrayList<State> movesAsState = this.state.getPossibleMoves();
+    Iterator<State> iterator = movesAsState.iterator();
+    while(iterator.hasNext()){
+      this.appendKnot(iterator.next());
+    }
+    return this.children;
+  }
 
+  int max(int depth){
+    if(depth == 0 || this.state.isTerminal()){
+      return this.state.heuristic();
+    }
+    int maxWert = -100000000;
+    ArrayList<Knoten> posMoves = getPossibleMovesAsKnots();
+    Iterator<Knoten> iterator = posMoves.iterator();
+    while(iterator.hasNext()){
+      Knoten k = iterator.next();
+      int wert = k.min(depth-1);
+      if(wert > maxWert){
+        maxWert = wert;
+        if(depth == finalDepth){
+          bestNextMove = k;
+        }
+      }
+    }
+    return maxWert;
+  }
+
+  int min(int depth){
+    if(depth == 0 || this.state.isTerminal()){
+      return this.state.heuristic();
+    }
+    int minWert = +100000000;
+    ArrayList<Knoten> posMoves = getPossibleMovesAsKnots();
+    Iterator<Knoten> iterator = posMoves.iterator();
+    while(iterator.hasNext()){
+      Knoten k = iterator.next();
+      int wert = k.max(depth-1);
+      if(wert < minWert){
+        minWert = wert;
+        if(depth == finalDepth){
+          bestNextMove = k;
+        }
+      }
+    }
+    return minWert;
+  }
+
+  static State letKIroll(State state){
+    bestNextMove = null;
+    Knoten startingKnot = getFirstKnot(state);
+    startingKnot.max(finalDepth);
+    if(bestNextMove == null){
+      System.out.println("Its over...");
+    }
+    return bestNextMove.state;
+  }
+
+  public static void main(String[] args){
+    State state = new State();
+
+    int moves = 0;
+    while(!state.isTerminal() && moves < 60){
+      moves++;
+      state = letKIroll(state);
+      System.out.println(state);
+      state = someNextMove(state);
+      System.out.println();
+      System.out.println(state.getGameStats());
+      System.out.println(state);
+    }
+  }
+
+  static State someNextMove(State state){
+    return state.getPossibleMoves().get(2);
+  }
 
 
 }
