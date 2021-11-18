df85285e68f6279be22e7d4b476a540bd482484f
jawinter <jawinter@mail.uni-mannheim.de>
2021-11-18 13:12:43 +0100

    extendKB done

--- Minesweeper/src/LogicalMSAgent.java
+++ Minesweeper/src/LogicalMSAgent.java
@@ -1,6 +1,7 @@
 package Minesweeper.src;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
 import org.sat4j.specs.IProblem;
@@ -15,12 +16,34 @@ public class LogicalMSAgent extends MSAgent {
   final int NBCLAUSES = 500000;
   private int numOfRows;
   private int numOfCols;
+  private ArrayList<Integer> safeTiles;
 
   public LogicalMSAgent(MSField field) {
     super(field);
     MAXVAR = field.getNumOfCols() * field.getNumOfRows();
     numOfRows = this.field.getNumOfRows();
     numOfCols = this.field.getNumOfCols();
+    KB = new ArrayList<int[]>();
+  }
+
+  public static void main(String[] args) {
+    printTruthTable(8);
+    MSField f = new MSField("Minesweeper/fields/" + UsageExample.fields[5]);
+    LogicalMSAgent testAgent = new LogicalMSAgent(f);
+    int count = 1;
+    for(int i = 0;i < testAgent.numOfRows;i++){
+      for(int j = 0;j < testAgent.numOfCols;j++){
+        System.out.print(String.format("%2d",count++) + "\t");
+      }
+      System.out.println();
+    }
+    System.out.println(testAgent.findName(2,3));
+    ArrayList<Integer> huso = testAgent.getNeighbours(2,3);
+    Iterator<Integer> it = huso.iterator();
+    while(it.hasNext()) {
+      System.out.print(it.next() + " ");
+    }
+    testAgent.extendKB(2,1,1);
   }
 
   @Override
@@ -80,93 +103,137 @@ public class LogicalMSAgent extends MSAgent {
     }
   }
 
+  /** Gibt für Feld an Position x,y den Namen zurück.
+   * Oben links angefangen bei 1 und von links nach rechts hochzählend.
+   */
+  public int findName(int x,int y){
+    return x + y * numOfCols + 1;
+  }
+
+  /** Nimmt x,y und gibt alle Nachbarn um das Feld als AraryList mit int zurück.
+   * Die Zahl steht dabei für den Namen des Feldes. Angefangen oben links bei 1 und von links nach rechts dann inkrementierend.
+   */
   private ArrayList<Integer> getNeighbours(int x, int y){
     ArrayList<Integer> neighbours = new ArrayList<Integer>();
 
     if(x == 0 && y == 0){
       // linke obere Ecke
-      neighbours.add(Tile.findName(1,0));
-      neighbours.add(Tile.findName(1,1));
-      neighbours.add(Tile.findName(0,1));
+      neighbours.add(this.findName(1,0));
+      neighbours.add(this.findName(1,1));
+      neighbours.add(this.findName(0,1));
     } else if(x == 0 && y == numOfRows){
       // linke untere Ecke
-      neighbours.add(Tile.findName(0, y-1));
-      neighbours.add(Tile.findName(1,y-1));
-      neighbours.add(Tile.findName(1,y));
+      neighbours.add(this.findName(0, y-1));
+      neighbours.add(this.findName(1,y-1));
+      neighbours.add(this.findName(1,y));
     } else if(x == numOfCols && y == 0){
       // rechte obere Ecke
-      neighbours.add(Tile.findName(x-1,0));
-      neighbours.add(Tile.findName(x-1,1));
-      neighbours.add(Tile.findName(x,1));
+      neighbours.add(this.findName(x-1,0));
+      neighbours.add(this.findName(x-1,1));
+      neighbours.add(this.findName(x,1));
     } else if(x == numOfCols && y == numOfRows){
       // recht untere Ecke
-      neighbours.add(Tile.findName(x,y-1));
-      neighbours.add(Tile.findName(x-1,y-1));
-      neighbours.add(Tile.findName(x-1,y));
+      neighbours.add(this.findName(x,y-1));
+      neighbours.add(this.findName(x-1,y-1));
+      neighbours.add(this.findName(x-1,y));
     } else if(x == 0){
       // linker Rand
-      neighbours.add(Tile.findName(0,y-1));
-      neighbours.add(Tile.findName(1,y-1));
-      neighbours.add(Tile.findName(1,y));
-      neighbours.add(Tile.findName(1,y+1));
-      neighbours.add(Tile.findName(0,y+1));
+      neighbours.add(this.findName(0,y-1));
+      neighbours.add(this.findName(1,y-1));
+      neighbours.add(this.findName(1,y));
+      neighbours.add(this.findName(1,y+1));
+      neighbours.add(this.findName(0,y+1));
     } else if(x == numOfCols){
       // rechter Rand
-      neighbours.add(Tile.findName(x,y-1));
-      neighbours.add(Tile.findName(x,y+1));
-      neighbours.add(Tile.findName(x-1,y-1));
-      neighbours.add(Tile.findName(x-1,y));
-      neighbours.add(Tile.findName(x-1,y+1));
+      neighbours.add(this.findName(x,y-1));
+      neighbours.add(this.findName(x,y+1));
+      neighbours.add(this.findName(x-1,y-1));
+      neighbours.add(this.findName(x-1,y));
+      neighbours.add(this.findName(x-1,y+1));
     } else if(y == 0){
       // oberer Rand
-      neighbours.add(Tile.findName(x-1,y));
-      neighbours.add(Tile.findName(x-1,y+1));
-      neighbours.add(Tile.findName(x,y+1));
-      neighbours.add(Tile.findName(x+1,y+1));
-      neighbours.add(Tile.findName(x+1,y));
+      neighbours.add(this.findName(x-1,y));
+      neighbours.add(this.findName(x-1,y+1));
+      neighbours.add(this.findName(x,y+1));
+      neighbours.add(this.findName(x+1,y+1));
+      neighbours.add(this.findName(x+1,y));
     } else if(y == numOfRows){
       // unterer Rand
-      neighbours.add(Tile.findName(x-1,y));
-      neighbours.add(Tile.findName(x-1,y-1));
-      neighbours.add(Tile.findName(x,y-1));
-      neighbours.add(Tile.findName(x+1,y-1));
-      neighbours.add(Tile.findName(x+1,y));
+      neighbours.add(this.findName(x-1,y));
+      neighbours.add(this.findName(x-1,y-1));
+      neighbours.add(this.findName(x,y-1));
+      neighbours.add(this.findName(x+1,y-1));
+      neighbours.add(this.findName(x+1,y));
     } else {
       // das Ding ist in der Mitte
-      neighbours.add(Tile.findName(x-1,y-1));
-      neighbours.add(Tile.findName(x,y-1));
-      neighbours.add(Tile.findName(x+1,y-1));
-      neighbours.add(Tile.findName(x-1,y));
-      neighbours.add(Tile.findName(x+1,y));
-      neighbours.add(Tile.findName(x-1,y+1));
-      neighbours.add(Tile.findName(x,y+1));
-      neighbours.add(Tile.findName(x+1,y+1));
+      neighbours.add(this.findName(x-1,y-1));
+      neighbours.add(this.findName(x,y-1));
+      neighbours.add(this.findName(x+1,y-1));
+      neighbours.add(this.findName(x-1,y));
+      neighbours.add(this.findName(x+1,y));
+      neighbours.add(this.findName(x-1,y+1));
+      neighbours.add(this.findName(x,y+1));
+      neighbours.add(this.findName(x+1,y+1));
     }
     return neighbours;
   }
 
-  public void extendKB(int feedback,int x,int y) {
-    int neighbours = 0;
-    if(feedback>=0){
-      KB.add(new int[] {x + y * numOfCols + 1});
-      //Man hat entweder 3,5 oder 8 Nachbarn
-      int tile = x + y * numOfCols + 1;
-      if(tile==1 || tile == (numOfCols * numOfRows) || tile == (numOfCols) || tile == (numOfCols * numOfRows - (numOfCols-1))) {
-        // Eckfeld -> 3 Nachbarn
-        neighbours = 3;
-        ArrayList<Integer> allNeighbours = getNeighbours(x,y);
-        // 0 1 2 3 Minen drum herum
+  /** Gibt 2D-Feld von int zurück, welches alle Interpretationen für n Variablen als Tabelle darstellt.
+   */
+  private static int[][] printTruthTable(int n) {
+    int rows = (int) Math.pow(2,n);
+    int [][] truthTable = new int[rows][n];
 
-      } else if (x == 0 || y == 0 || x == numOfCols || y == numOfRows){
-        //Randfeld -> 5 Nachbarn
-        neighbours = 5;
-        ArrayList<Integer> allNeighbours = getNeighbours(x,y);
-      } else {
-        //Mitte -> 8 Nachbarn
-        neighbours = 8;
-        ArrayList<Integer> allNeighbours = getNeighbours(x,y);
+    for (int i=0; i<rows; i++) {
+      for (int j=0; j<n; j++) {
+        truthTable [i][n-j-1] = (i/(int)Math.pow(2, j))%2;
       }
     }
+    return truthTable;
+  }
+
+  /** Nimmt Wahrheitstabelle mit 1en und 0en und ersetzt in den Zeilen mit der passenden Variable
+   * tt = truthTable; n = neighbours
+   */
+  private void addVarNames(int[][] tt,ArrayList<Integer> n){
+    if(tt[0].length == n.size()) {
+      for(int i = 0;i<tt.length;i++){
+        for(int j = 0;j<tt[0].length;j++){
+          if(tt[i][j]==0) {
+            tt[i][j] = n.get(j);
+          } else {
+            tt[i][j] = n.get(j) * -1;
+          }
+        }
+      }
+    } else {
+
+    }
+  }
+
+  /**
+  Nimmt Wahrheitstabelle mit Variablen und schliesst die Zeilen aus, die Modelle sind.
+  Gibt ArrayList mit Klauseln für KNF zurück.
+   */
+  private ArrayList<int[]> extractModels(int[][] tt,int feedback){
+    ArrayList<int[]> KNF = new ArrayList<int[]>();
+  for(int[] row : tt){
+    int countNeg = 0;
+    for(int i : row){
+      if(i<0){countNeg++;}
+    }
+    if(countNeg!=feedback){KNF.add(row);}
+  }
+  return KNF;
+  }
+
+  /** Nimmt Feedback zu gedrücktem Feld x,y. Wandelt dies in Klauseln um, die in in die KB gespeist werden.
+   */
+  public void extendKB(int feedback,int x,int y) {
+    ArrayList<Integer> neighbours = this.getNeighbours(x,y);
+    int [][] truthTable = printTruthTable(neighbours.size());
+    addVarNames(truthTable,neighbours);
+    KB.addAll(extractModels(truthTable,feedback));
   }
 
   @Override
